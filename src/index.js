028373dbd0586a9002e6b029d5317f5769b1e414
valeria_gora <valeriia.gora@jmind.systems>
2020-12-26 19:41:19 +0200

    boilerplate

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+// import store from 'store';
+
+import 'normalize.css';
+import './styles.scss';
+
+const App = () => (
+   <div>App</div>
+);
+
+/* eslint-disable no-undef */
+ReactDOM.render(<App />, document.querySelector('#root'));
