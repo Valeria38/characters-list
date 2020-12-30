ac1a21cae8f1ac3c0f7a9bfff5fd16642593822d
valeria_gora <valeriia.gora@jmind.systems>
2020-12-30 12:06:49 +0200

    _redirects

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from 'features/App';
 
@@ -11,9 +11,9 @@ import './styles.scss';
 
 const Root = () => (
   <Provider store={store}>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </Provider>
 );
 
